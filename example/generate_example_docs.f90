4d438a3b711a7e7a3c51d31d1836ee39dd3052c8
Christopher Albert <albert@tugraz.at>
2025-09-01 09:22:17 +0200

    refactor(docs): consolidate documentation modules into core/processing/output (fixes #942)
    - Replace 9 scattered fortplot_doc_* modules with 3 focused modules
      - : constants + file/path + shared text utilities
      - : examples + README processor + source links
      - : media scanning + output rendering
    - Update example driver to new APIs
    - Remove redundant modules to reduce bloat and dependencies
    
    All tests pass locally (make test-ci, pytest, make test).

--- example/generate_example_docs.f90
+++ example/generate_example_docs.f90
@@ -1,23 +1,22 @@
 program generate_example_docs
     !! Generate FORD documentation pages for fortplot examples
-    use fortplot_doc_constants, only: PATH_MAX_LEN
-    use fortplot_doc_examples, only: get_example_count, get_example_dir, get_example_name
-    use fortplot_doc_processor, only: process_example
+    use fortplot_doc_core, only: PATH_MAX_LEN
+    use fortplot_doc_processing, only: get_example_count, get_example_dir, get_example_name, process_example
     implicit none
-    
+
     character(len=PATH_MAX_LEN) :: example_dir, example_name
     integer :: n_examples, i
-    
+
     print *, "Generating example documentation..."
-    
+
     n_examples = get_example_count()
-    
+
     do i = 1, n_examples
         call get_example_dir(i, example_dir)
         call get_example_name(i, example_name)
         call process_example(trim(example_dir), trim(example_name))
     end do
-    
+
     print *, "Documentation generation complete!"
 
 end program generate_example_docs
