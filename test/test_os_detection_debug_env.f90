17eeb753637e574152db5d208b58a7eabf3beadd
Christopher Albert <albert@tugraz.at>
2025-09-01 23:17:47 +0200

    test: make debug env test portable on Windows by using putenv runtime dispatch (fixes CI windows-latest failure)

--- test/test_os_detection_debug_env.f90
+++ test/test_os_detection_debug_env.f90
@@ -1,11 +1,12 @@
 program test_os_detection_debug_env
-    !! Verify is_debug_enabled reads FORTPLOT_DEBUG
+    !! Verify is_debug_enabled reads FORTPLOT_DEBUG (cross-platform)
 
     use, intrinsic :: iso_c_binding, only: c_int, c_char, c_null_char
     use fortplot_os_detection, only: is_debug_enabled
 
     implicit none
 
+    ! Provide both POSIX and Windows interfaces; dispatch at runtime.
     interface
         function c_setenv(name, value, overwrite) bind(C, name="setenv") result(ret)
             import :: c_int, c_char
@@ -19,6 +20,11 @@ program test_os_detection_debug_env
             character(kind=c_char), dimension(*) :: name
             integer(c_int) :: ret
         end function c_unsetenv
+        function c_putenv(s) bind(C, name="putenv") result(ret)
+            import :: c_int, c_char
+            character(kind=c_char), dimension(*) :: s
+            integer(c_int) :: ret
+        end function c_putenv
     end interface
 
     call ensure(.true., 'sanity')
@@ -40,17 +46,33 @@ program test_os_detection_debug_env
 contains
 
     subroutine set_env(k, v)
+        use fortplot_os_detection, only: is_windows
         character(len=*), intent(in) :: k, v
         integer(c_int) :: rc
-        rc = c_setenv(trim(k)//c_null_char, trim(v)//c_null_char, 1_c_int)
-        call ensure(rc == 0, 'setenv succeeded for '//trim(k))
+        character(len=:), allocatable :: kv
+        if (is_windows()) then
+            kv = trim(k)//'='//trim(v)
+            rc = c_putenv(kv//c_null_char)
+            call ensure(rc == 0, 'putenv succeeded for '//trim(k))
+        else
+            rc = c_setenv(trim(k)//c_null_char, trim(v)//c_null_char, 1_c_int)
+            call ensure(rc == 0, 'setenv succeeded for '//trim(k))
+        end if
     end subroutine set_env
 
     subroutine unset_env(k)
+        use fortplot_os_detection, only: is_windows
         character(len=*), intent(in) :: k
         integer(c_int) :: rc
-        rc = c_unsetenv(trim(k)//c_null_char)
-        call ensure(rc == 0, 'unsetenv succeeded for '//trim(k))
+        character(len=:), allocatable :: kv
+        if (is_windows()) then
+            kv = trim(k)//'='
+            rc = c_putenv(kv//c_null_char)
+            call ensure(rc == 0, 'putenv unset succeeded for '//trim(k))
+        else
+            rc = c_unsetenv(trim(k)//c_null_char)
+            call ensure(rc == 0, 'unsetenv succeeded for '//trim(k))
+        end if
     end subroutine unset_env
 
     subroutine ensure(cond, msg)
