92d366bbbc22057a7148469bf0c4d798dafa65b1
Christopher Albert <albert@tugraz.at>
2025-09-02 04:40:34 +0200

    cleanup(streamplot): remove unused add_simple_streamline helper

--- src/figures/plot_types/fortplot_figure_streamlines.f90
+++ src/figures/plot_types/fortplot_figure_streamlines.f90
@@ -10,7 +10,7 @@ module fortplot_figure_streamlines
     implicit none
     
     private
-    public :: add_simple_streamline, clear_streamline_data
+    public :: clear_streamline_data
     public :: streamplot_basic_validation, streamplot_figure
     
 contains
@@ -33,40 +33,6 @@ contains
             return
         end if
     end function streamplot_basic_validation
-    
-    subroutine add_simple_streamline(x, y, u, v, line_color, &
-                                    stream_x, stream_y, stream_color)
-        !! Add a simple streamline to demonstrate functionality
-        !! This creates a basic horizontal streamline that shows
-        !! streamplot is working for the test suite.
-        real(wp), intent(in) :: x(:), y(:)
-        real(wp), intent(in) :: u(:,:), v(:,:)
-        real(wp), intent(in), optional :: line_color(3)
-        real(wp), allocatable, intent(out) :: stream_x(:), stream_y(:)
-        real(wp), intent(out) :: stream_color(3)
-        
-        integer :: i, n_points
-        real(wp) :: x_start, y_start, dx
-        
-        ! Set default streamline color (blue)
-        stream_color = [0.0_wp, 0.447_wp, 0.698_wp]
-        if (present(line_color)) stream_color = line_color
-        
-        ! Create a simple horizontal streamline through the middle of the domain
-        n_points = size(x)
-        allocate(stream_x(n_points), stream_y(n_points))
-        
-        ! Start at the leftmost x position, middle y position
-        x_start = x(1)
-        y_start = (y(1) + y(size(y))) / 2.0_wp
-        dx = (x(size(x)) - x(1)) / real(n_points - 1, wp)
-        
-        ! Create a simple streamline (horizontal line for now)
-        do i = 1, n_points
-            stream_x(i) = x_start + real(i - 1, wp) * dx
-            stream_y(i) = y_start
-        end do
-    end subroutine add_simple_streamline
     
     subroutine clear_streamline_data(streamlines)
         !! Clear streamline data
