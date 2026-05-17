bc38db085be75ae833454ff8611374f8f886f551
Christopher Albert <albert@tugraz.at>
2026-05-17 07:22:11 +0200

    fix: orchestrate rescue commit

---
+++ test/test_all_modules.f90
@@ -0,0 +1,15 @@
+module test_all_modules
+    use fortplot_imagemagick
+    use fortplot_png_validation
+    use fortplot_spy_backend
+    use fortplot_test_helpers
+    use fortplot_testing
+    use test_output_helpers
+    use test_pdf_utils
+end module test_all_modules
+
+program test_all_modules_main
+    use test_all_modules
+    implicit none
+    print *, "All support modules compiled successfully"
+end program test_all_modules_main
